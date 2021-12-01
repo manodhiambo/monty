b0c594590cc5faba50a88bceaa4600c52e22b4a5
manodhiambo <manodhiambo@gmail.com>
2021-12-01 08:35:21 -0800

    fix

--- opcode_funcs.c
+++ opcode_funcs.c
@@ -6,25 +6,24 @@
  */
 void stack_add(stack_t **head, unsigned int line_num)
 {
-stack_t *tmp = NULL;
-int nel = 0;
+	stack_t *tmp = NULL;
+	int nel = 0;
 
-tmp = *head;
-while (tmp)
-{
-nel++;
-tmp = tmp->next;
-}
-if (nel < 2)
-{
-dprintf(2, "L%u: can't add, stack too short\n", line_num);
-
-free_vars();
-exit(EXIT_FAILURE);
-}
-tmp = (*head)->next;
-tmp->n += (*head)->n;
-stack_pop(head, line_num);
+	tmp = *head;
+	while (tmp)
+	{
+		nel++;
+		tmp = tmp->next;
+	}
+	if (nel < 2)
+	{
+		dprintf(2, "L%u: can't add, stack too short\n", line_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->next;
+	tmp->n += (*head)->n;
+	stack_pop(head, line_num);
 }
 /**
  * stack_nop - Doesn't do anything
@@ -33,8 +32,8 @@ stack_pop(head, line_num);
  */
 void stack_nop(stack_t **head, unsigned int line_num)
 {
-(void)head;
-(void)line_num;
+	(void)head;
+	(void)line_num;
 }
 /**
  * stack_sub - subtracts the top element of stack from the second element
@@ -43,24 +42,24 @@ void stack_nop(stack_t **head, unsigned int line_num)
  */
 void stack_sub(stack_t **head, unsigned int line_num)
 {
-stack_t *tmp = NULL;
-int nel = 0;
+	stack_t *tmp = NULL;
+	int nel = 0;
 
-tmp = *head;
-while (tmp)
-{
-nel++;
-tmp = tmp->next;
-}
-if (nel < 2)
-{
-dprintf(2, "L%u: can't sub, stack too short\n", line_num);
-free_vars();
-exit(EXIT_FAILURE);
-}
-tmp = (*head)->next;
-tmp->n -= (*head)->n;
-stack_pop(head, line_num);
+	tmp = *head;
+	while (tmp)
+	{
+		nel++;
+		tmp = tmp->next;
+	}
+	if (nel < 2)
+	{
+		dprintf(2, "L%u: can't sub, stack too short\n", line_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->next;
+	tmp->n -= (*head)->n;
+	stack_pop(head, line_num);
 }
 /**
  * stack_queue - sets the format of the data to a FIFO stack
@@ -69,10 +68,10 @@ stack_pop(head, line_num);
  */
 void stack_queue(stack_t **head, unsigned int line_num)
 {
-(void)head;
-(void)line_num;
+	(void)head;
+	(void)line_num;
 
-vars.order = 0;
+	vars.order = 0;
 }
 
 /**
@@ -82,8 +81,8 @@ vars.order = 0;
  */
 void order_stack(stack_t **head, unsigned int line_num)
 {
-(void)head;
-(void)line_num;
+	(void)head;
+	(void)line_num;
 
-vars.order = 1;
+	vars.order = 1;
 }
